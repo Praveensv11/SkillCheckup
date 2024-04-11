973c1efc5d03d033cdd260301d570bbe6a478f1a
Praveen <praveensv799@gmail.com>
2024-04-11 16:35:48 +0530

    aptitude requirements

--- interview_project/interview_project/settings.py
+++ interview_project/interview_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'aptitude',
+    'hr',
+    'technical',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
